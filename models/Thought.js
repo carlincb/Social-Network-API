59c9bb5b1bfb8950acd65bed153221219b8a2904
carlincb <carlin.colleen@gmail.com>
2022-01-11 20:05:21 -0700

    Worked on Thought.js.

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = require('./Reaction')
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    //   get: (date) => {
+
+    //   }
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(()=> this.reactions.length);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
