ee8768521d58ec3cdbe848c8811075024d4fb179
carlincb <carlin.colleen@gmail.com>
2022-01-13 21:38:34 -0700

    Completed userRoutes.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,10 +3,17 @@ const router = require('express').Router();
 const {
     getUsers,
     getSingleUser,
+    createUser,
+    deleteUser,
+    updateUser,
+    addFriend,
+    removeFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
